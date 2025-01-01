acc45b9bcd2b763519b23e31d078f99cb5ed0650
suraj <surajsuthar0067@gmail.com>
2025-01-01 19:48:12 +0530

    added google and github auth2.0

--- auth.ts
+++ auth.ts
@@ -1,9 +1,35 @@
-import NextAuth from "next-auth"
+import NextAuth , { type DefaultSession } from "next-auth"
 import { PrismaAdapter } from "@auth/prisma-adapter"
 import authConfig from "./auth.config"
 import { db } from "@/lib/db"
- 
-export const { auth, handlers, signIn, signOut } = NextAuth({
+import { getUserById } from "./data"
+import { UserRole } from "@prisma/client"
+
+export const { 
+  auth, 
+  handlers, 
+  signIn, 
+  signOut
+} = NextAuth({
+  callbacks : {
+    async session({session , token}) {
+      console.log(token)
+      if(token.sub && session.user) {
+        session.user.id = token.sub
+      }
+      if(token.role && session.user) {
+        session.user.role = token.role as UserRole
+      }
+
+    },
+    async jwt({ token }) {
+      if(!token.sub) return token;
+      const loggedUser = await getUserById(token.sub)
+      if(!loggedUser) return token;
+      token.role = loggedUser.role
+      return token;
+    }
+  },
   adapter: PrismaAdapter(db),
   session: { strategy: "jwt" },
   ...authConfig,
